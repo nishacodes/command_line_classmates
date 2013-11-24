cd30e25af395cfe484089d8827f56696669bbf24
nishacodes <nishabatra27@gmail.com>
2013-11-24 14:40:51 -0500

    organize everything into a class

--- app.rb
+++ app.rb
@@ -3,40 +3,41 @@ require './lib/student.rb' # requires all files in lib directory
 require 'launchy'
 
 
-
-	student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
-
-	names = student_scraper.get_student_names
-	twitters = student_scraper.get_twitter
-	blogs = student_scraper.get_blogs
-
-	students =[]
-	28.times do |i|
-		students << Student.new(names[i],twitters[i],blogs[i])
-	end
-
-
 class GetStudent
 
-	attr_reader :student
+	attr_accessor :scraper, :names, :twitters, :blogs, :students, :request
 
 	def initialize
-		@student
+		scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+		@names = scraper.get_student_names
+		@twitters = scraper.get_twitter
+		@blogs = scraper.get_blogs
+		@students = []
+		@request
 	end
 
 	def run
+		storeInfo
 		askInfo
 		printInfo
 	end
 
+	def storeInfo
+		28.times do |i|
+			@students << Student.new(@names[i],@twitters[i],@blogs[i])
+		end
+	end
+
+	
+
 	def askInfo
 		puts "Enter the name of the person you want to look up. Or enter 'all' to see everyone."
-		@student = gets.chomp
+		@request = gets.chomp
 	end
 
 	def printInfo
-		if @student == "all" 
-			students.each do |student|
+		if @request == "all" 
+			@students.each do |student|
 				puts student.name
 				puts student.twitter
 				puts student.blog
