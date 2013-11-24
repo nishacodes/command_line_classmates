fa90900992d9c6235ce1e0ee94da56faa4c96d28
nishacodes <nishabatra27@gmail.com>
2013-11-24 18:41:54 -0500

    reformat twitter handles into strings, control flow for any twitter or blog requests

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'launchy'
 
 class GetStudent
 
-	attr_accessor :scraper, :names, :twitters, :blogs, :students, :request
+	attr_accessor :names, :twitters, :blogs, :students, :request
 
 	def initialize
 		scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
@@ -18,8 +18,10 @@ class GetStudent
 
 	def run
 		storeInfo
+		formatLinks
+		showOptions
 		askInfo
-		printInfo
+		processRequest
 	end
 
 	def storeInfo
@@ -28,13 +30,65 @@ class GetStudent
 		end
 	end
 
-	
+	def formatLinks
+		@twitters.delete("none")
+		@twitters.each {|handle| handle.gsub!("@","http://www.twitter.com/") }
+		@blogs.delete("none")
+	end
 
+	def showOptions
+		puts "-" * 60
+		puts "************ FLATIRON SCHOOL STUDENT DIRECTORY ************"
+		puts "Please choose from the below options:"
+		puts "-" * 60
+		puts "'[name] -b'   to launch that student's blog"
+		puts "'[name] -t'   to launch that student's twitter"
+		puts "'any -b'      to launch a random student's blog"
+		puts "'any -t'      to launch a random student's twitter"
+		puts "'all'         to view all student info"
+		puts "-" * 60
+	end
+	
 	def askInfo
-		puts "Enter the name of the person you want to look up. Or enter 'all' to see everyone."
+		puts "Please enter your choice."
 		@request = gets.chomp
 	end
 
+	def processRequest
+		if @request.start_with?('any')
+			launchRandom
+		elsif @request == "all"
+			puts "printinfo"
+			# printInfo
+		elsif @request.end_with?('-b') || @request.end_with?('-t')
+			puts "byname"
+			# launchByName
+		else
+			puts "Sorry that is not a valid option. Try again."
+			@request = gets.chomp
+			processRequest
+		end
+	end
+
+	def launchByName
+		puts "hi"
+		# case
+		# 	when @request == "any -t"
+				
+		# 	when @request == "any -b"
+				
+		# 	end
+	end
+
+	def launchRandom
+		case
+			when @request == "any -b"
+				Launchy.open("#{@blogs.sample}")
+			when @request == "any -t"
+				Launchy.open("#{@twitters.sample}")
+			end
+	end
+	
 	def printInfo
 		if @request == "all" 
 			@students.each do |student|
