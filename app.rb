0cd519d69faa1e49c0545df19269657a14803240
nishacodes <nishabatra27@gmail.com>
2013-11-24 12:28:41 -0500

    set up initial structure of GetStudent class

--- app.rb
+++ app.rb
@@ -1,27 +1,64 @@
 require './lib/scraper.rb'
 require './lib/student.rb' # requires all files in lib directory
+require 'launchy'
 
-# 1) make a new scraper
-# 2) get student names from scraper
-# 3) get the blogs
-# 4) get the twitter
-# 5) make a new student object for each one
 
-student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
 
-names = student_scraper.get_student_names
-twitters = student_scraper.get_twitter
-blogs = student_scraper.get_blogs
+	student_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
 
-students =[]
-28.times do |i|
-	students << Student.new(names[i],twitters[i],blogs[i])
+	names = student_scraper.get_student_names
+	twitters = student_scraper.get_twitter
+	blogs = student_scraper.get_blogs
+
+	students =[]
+	28.times do |i|
+		students << Student.new(names[i],twitters[i],blogs[i])
+	end
+
+
+class GetStudent
+
+	attr_reader :student
+
+	def initialize
+		@student
+	end
+
+	def run
+		askInfo
+		printInfo
+	end
+
+	def askInfo
+		puts "Enter the name of the person you want to look up. Or enter 'all' to see everyone."
+		@student = gets.chomp
+	end
+
+	def printInfo
+		if @student == "all" 
+			students.each do |student|
+				puts student.name
+				puts student.twitter
+				puts student.blog
+				puts "***********"
+			end
+		end
+
+	end
+
+
+
 end
 
-students.each do |student|
-	puts student.name
-	puts student.twitter
-	puts student.blog
-	puts "***********"
-end
+
+
+
+app = GetStudent.new
+app.run
+
+
+
+
+
+
 
